f8ae349d23c87c9a40a61bd82af3b57db87b58c5
Luca Casonato <lucacasonato@yahoo.com>
2020-10-12 11:25:07 +0200

    refactor: rewrote routing logic

--- example/deps.ts
+++ example/deps.ts
@@ -1,5 +1,6 @@
 export { Fragment, h } from "../deps/preact/mod.ts";
 export type { JSX } from "../deps/preact/mod.ts";
+export { useLocation } from "../mod.ts";
 export type {
   AppProps,
   DocumentProps,
